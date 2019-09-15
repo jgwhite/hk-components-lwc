6a3d406022fd14a1157de1c75603aa3ab4a16dea
Matt Rothenberg <mattrothenberg@gmail.com>
2019-09-15 11:44:36 -0400

    Implement danger variant

--- src/modules/hk/menuItem/menuItem.js
+++ src/modules/hk/menuItem/menuItem.js
@@ -1,5 +1,12 @@
 import { LightningElement, api } from 'lwc';
 
 export default class HKMenuItem extends LightningElement {
+    @api variant;
     @api handleSelect = () => {};
+
+    get computedClass() {
+        const base = `hk-menu-item`;
+        const suffix = this.variant ? `--${this.variant}` : ``;
+        return `${base}${suffix}`;
+    }
 }
--- src/modules/hk/menuLink/menuLink.js
+++ src/modules/hk/menuLink/menuLink.js
@@ -1,6 +1,13 @@
 import { LightningElement, api } from 'lwc';
 
 export default class HKMenuLink extends LightningElement {
-  @api href
-  @api target
+    @api href;
+    @api target;
+    @api variant;
+
+    get computedClass() {
+        const base = `hk-menu-link`;
+        const suffix = this.variant ? `--${this.variant}` : ``;
+        return `${base}${suffix}`;
+    }
 }
