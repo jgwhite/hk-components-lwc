2e4f1a7963a2424874f87ec370804667b2961a57
Jamie White <jamie@jgwhite.co.uk>
2019-09-26 15:24:20 +0200

    Expose <hk-*>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import { buildCustomElementConstructor } from 'lwc';
-import MyApp from 'my/app';
+import HkMenu from 'hk/menu';
+import HkMenuButton from 'hk/menuButton';
+import HkMenuDivider from 'hk/menuDivider';
+import HkMenuItem from 'hk/menuItem';
+import HkMenuLink from 'hk/menuLink';
+import HkMenuList from 'hk/menuList';
 
-customElements.define('my-app', buildCustomElementConstructor(MyApp));
+customElements.define('hk-menu', buildCustomElementConstructor(HkMenu));
+customElements.define(
+    'hk-menu-button',
+    buildCustomElementConstructor(HkMenuButton)
+);
+customElements.define(
+    'hk-menu-divider',
+    buildCustomElementConstructor(HkMenuDivider)
+);
+customElements.define(
+    'hk-menu-item',
+    buildCustomElementConstructor(HkMenuItem)
+);
+customElements.define(
+    'hk-menu-link',
+    buildCustomElementConstructor(HkMenuLink)
+);
+customElements.define(
+    'hk-menu-list',
+    buildCustomElementConstructor(HkMenuList)
+);
